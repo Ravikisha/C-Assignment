7fcd90ff7b88f81acfaf02c1ca7a0febbcb949e2
ravi kishan <ravikishan63392@gmail.com>
2021-12-30 00:08:45 +0530

    Question 20 add

---
+++ Ques20.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+    char text[50];
+    int size = 0,status_code = 0;
+    scanf("%s",&text);
+    size = strlen(text);
+    int mid = (size / 2)+1;
+    for (int z = 0; z < size; z++)
+    {
+        if (text[z] != 't' || text[z] != 'T')
+        {
+            status_code = -1;
+        }
+    }
+    for (int i = 0; i < mid; i++)
+    {
+        if (text[i] == 't' || text[i] == 'T')
+        {
+            status_code = 1;
+            break;
+        }
+        
+    }
+    for (int j = mid; j < size; j++)
+    {
+        if (text[j] == 't' || text[j] == 'T')
+        {
+            status_code = 2;
+            break;
+        }
+        
+    }
+    printf("%d",status_code);
+    
+    
+}
